2c65441b7005374e2ec34860f7ba79ba55a0c84f
maximdushniak <maxim.dushniak@gmail.com>
2016-10-06 22:30:33 +0300

    проверка на наличие спец символов и чисел

--- password_strength.py
+++ password_strength.py
@@ -1,17 +1,35 @@
+import re
 
+
+def contain_lower(password):
+    return re.search(r'.*[a-z]+.*', password)
+
+
+def contain_upper(password):
+    return re.search(r'.*[A-Z]+.*', password)
+
+
+def contain_numeric(password):
+    return re.search(r'.*[0-9]+.*', password)
+
+
+def contain_special(password):
+    return re.search(r'.*[~!@#$%^&*()_+\-=\{\}\[\]:;\`<>\.\/\\]+.*', password)
 
 def get_password_strength(password):
     # password = 'sdjfiksjdfFDSAFfdsa3424323'
     password_strength = 1
-    upper = False
-    lower = False
-    for i in password:
-        if i.islower():
-            lower = True
-        if i.isupper():
-            upper = True
 
-    if upper and lower:
+    if contain_lower(password):
+        password_strength += 1
+
+    if contain_upper(password):
+        password_strength += 1
+
+    if contain_numeric(password):
+        password_strength += 1
+
+    if contain_special(password):
         password_strength += 1
 
     return password_strength
